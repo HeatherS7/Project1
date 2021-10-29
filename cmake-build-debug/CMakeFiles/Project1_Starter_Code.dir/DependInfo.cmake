12135d19d64d6aa9e53f995b028c8969bc512688
HeatherS7 <heathernschone@gmail.com>
2021-10-28 21:16:13 -0600

    Adding some stuff

---
+++ cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/AddAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/AddAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/ColonAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/ColonDashAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/ColonDashAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/CommaAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/CommaAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/CommentAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/CommentAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Database.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Database.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/DatalogProgram.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DatalogProgram.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/FactsAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/FactsAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Header.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Header.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/IdAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/IdAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Interpreter.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Interpreter.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Left_ParenAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Left_ParenAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Lexer.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/MultiplyAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/MultiplyAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Parameter.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parameter.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Parser.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parser.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/PeriodAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/PeriodAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Predicate.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Predicate.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Q_MarkAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Q_MarkAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/QueriesAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/QueriesAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Relation.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Relation.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Right_ParenAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Right_ParenAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Rule.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Rule.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/RulesAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/RulesAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/SchemesAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/SchemesAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/StringAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/StringAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Token.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Token.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/Tuple.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Tuple.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/UndefinedAutomaton.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/UndefinedAutomaton.cpp.obj"
+  "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/main.cpp" "C:/Users/heath/Documents/Programming/CS236Programs/Project1_Starter_Code/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../TestCases"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
